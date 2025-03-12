08e0cf92c3361d41e92d7f83d747f4a19549d9ca
Niranjan Patil <ropatil251@gmail.com>
2025-03-13 02:24:55 +0530

    Add files via upload

---
+++ src/Niranjan.jsx
@@ -0,0 +1,65 @@
+// import React from "react";
+// import "./Niranjan.css";
+// import "bootstrap/dist/css/bootstrap.min.css"; 
+
+// import Compo1 from "./Compo1";
+// import Compo2 from "./Compo2";
+// import Compo3 from "./Compo3";
+// import Compo4 from "./Compo4";
+// import Compo5 from "./Compo5";
+
+// const Niranjan = () => {
+//     return (
+//       <div>
+//         <Compo1/><hr/>
+//         <Compo2/><hr/>
+//         <Compo3/><hr/>
+//         <Compo4/><hr/>
+//         <Compo5/><hr/>
+//       </div>
+//     );
+//   };
+
+//   export default Niranjan;
+
+
+
+
+
+import React from "react";
+
+import Courses from "./Component/Courses";
+import Employees from "./Component/Employees";
+import Players from "./Component/Players";
+import Students from "./Component/Students";
+import Car from "./Component/Car";
+
+const Niranjan = () => {
+    return (
+      <div>
+        <Courses/><hr/>
+        <Employees/><hr/>
+        <Players/><hr/>
+        <Students/><hr/>
+        <Car/><hr/>
+      </div>
+    );
+  };
+
+  export default Niranjan;
+
+
+// import React from "react";
+// import UseState from "./UseState";
+
+// const Niranjan = () => {
+//     return (
+//       <div>
+//         <UseState/><hr/>
+//       </div>
+//     );
+//   };
+
+//   export default Niranjan;
+
+
